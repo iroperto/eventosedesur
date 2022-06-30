8d3e5084abee2cbec71d524a84bfdd85cbf285b2
Ivan Roperto <iropertoa@edesur.com.do>
2022-06-30 07:38:19 -0400

    preparacion nueva plantilla

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//servidor de la base de datos
+define("DB_HOST","localhost");
+
+//nombre de la base de datos
+define("DB_NAME","eventos_edesur");
+
+//usuario de la base de datos
+define("DB_USERNAME","dataadmin");
+
+//contraseña del usuaio de la Base de datos
+define("DB_PASSWORD", "Roiv@1976#");
+
+//Configuracion de los caracteres
+define("DB_ENCONDE","utf8");
+
+//nombre del proyecto
+define("PRO_NAME", "Eventos Edesur");
+?>
