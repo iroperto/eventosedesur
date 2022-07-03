18944f8f0207788efad21bf96f7ba78e718c75ae
Ivan Roperto <iropertoa@edesur.com.do>
2022-07-03 15:19:58 -0400

    correccion de estilos en plantilla

---
+++ vistas/footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.1.0
+        </div>
+        <strong>Copyright &copy; <?php echo date("Y") ; ?> <a href="www.edesur.com.do">Edesur Dominicana</a>.</strong> Todos los derechos reservados.
+    </footer>    
+    <!-- jQuery 2.1.4 -->
+    <script src="../public/js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../public/js/bootstrap.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../public/js/app.min.js"></script>
+  </body>
+</html>
